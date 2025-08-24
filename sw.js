df02c7579f09cde261a810f18889eb9a7419f31a
EpanE <34804803+EpanE@users.noreply.github.com>
2025-08-24 09:27:32 +0800

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'iir-app-v3';
+const ASSETS = [
+  './',
+  './index.html',
+  './app.js',
+  './worklet-processor.js',
+  './manifest.webmanifest?v=3',
+  './icons/icon-192.png',
+  './icons/icon-512.png',
+  './icons/favicon-32.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  if (e.request.method !== 'GET') return;
+  const url = new URL(e.request.url);
+  const sameOrigin = url.origin === location.origin;
+
+  e.respondWith(
+    caches.match(e.request).then(hit =>
+      hit || fetch(e.request).then(resp => {
+        if (resp.ok && sameOrigin) caches.open(CACHE).then(c => c.put(e.request, resp.clone()));
+        return resp;
+      }).catch(() => hit)
+    )
+  );
+});
